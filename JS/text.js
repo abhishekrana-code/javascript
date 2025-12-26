0092bb5b904e916ee124965d0e14cc86edba29ba
abhishekrana-code <iamabhishekrana07@gmail.com>
2025-12-26 12:06:25 +0530

    Add multiple HTML files and a CSS file with various examples and structures

---
+++ JS/text.js
@@ -0,0 +1,11 @@
+/*const product ={
+    title: "Ball Pen",
+    rating: 4,
+    price: 800,
+};
+console.log(product);*/
+
+// let username;
+// username = window.prompt("Enter your name");
+// console.log("Hello " + username);
+console.log("hello world");
